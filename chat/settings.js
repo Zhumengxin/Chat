993609861222543f1d1dc5b420df24e43bf1116a
PorridgeEater <fanhr5@gmail.com>
2016-06-26 19:48:01 +0800

    smartMenu

--- chat/settings.js
+++ chat/settings.js
@@ -6,7 +6,7 @@
             host: 'localhost',     //本地数据库
             port: '3306',
             user: 'root',          //数据库用户名
-            password: '123456',          //数据库密码
+            password: 'Mysql@1203',          //数据库密码
             database: 'chat'  //数据库名称
         }
     };
